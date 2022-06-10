3a3327ffb70517c67995d5bc958c004933a26c73
laurenbmcguire <102077139+laurenbmcguire@users.noreply.github.com>
2022-06-10 02:08:20 -0500

    Add files via upload
    made utilizing various online references

---
+++ ReactFirebaseApp/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB4-gzM77KNd1M8BoriU-Zn6ixk9eisCwE",
+    authDomain: "messing-app-lm.firebaseapp.com",
+    databaseURL: "https://messing-app-lm-default-rtdb.firebaseio.com",
+    projectId: "messing-app-lm",
+    storageBucket: "messing-app-lm.appspot.com",
+    messagingSenderId: "740072413249",
+    appId: "1:740072413249:web:ccccc654a577390ae88ec0"
+};
